0bcb8d0c89b747276d44d19a6f9dbd2b6d5ddae6
Deforris <jesse.muniz@deforris.com>
2023-09-30 08:10:28 -0400

    QOL and spelling mistake

--- ansible-ubuntu.sh
+++ ansible-ubuntu.sh
@@ -7,17 +7,21 @@
 #usage		 :bash ansible-ubuntu.sh
 #notes           :Installs bare minimum to perform ansible and starts ansible.
 #==============================================================================
-
-echo Deforris - Ubuntu Desktop Enviornment Setup
-
-echo Starting apt update.
+echo
+echo "********** Deforris - Ubuntu Desktop Enviornment Setup **********"
+echo
+echo "********** Starting apt update. **********"
 apt update -y
-echo Finished apt update.
-
-echo Starting apt upgrade.
+echo "********** Finished apt update. **********"
+echo
+echo "********** Starting apt upgrade. **********"
 apt upgrade -y
-echo Finished apt upgrade.
-
-echo Press Enter To Continue...
+echo "********** Finished apt upgrade. **********"
+echo
+echo "********** Starting apt autoremove. **********"
+apt autoremove -y
+echo "********** Finished apt autoremove. **********"
+echo
+echo "********** Press Enter To Continue To git Setup... **********"
 read
 bash git.sh
