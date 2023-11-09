383828eca6f8dbdf3a154256b0d8aaee9a012280
Jesse Muniz <142850309+deforris@users.noreply.github.com>
2023-11-09 07:32:17 -0500

    Added contents from ansible-ubuntu.sh to improve codebase

--- init.sh
+++ init.sh
@@ -3,8 +3,29 @@
 #description   :A clean starting point on automatic install.
 #author		     :deforris
 #date          :202301109
-#version       :1.0.0
+#version       :1.0.1
 #usage		     :bash init.sh
-#notes         :Starts git install and setup stuff which is not covered by ansible.
+#notes         :Starts git install and update stuff before ansible to prevent any conflicts.
 #==============================================================================
+echo
+echo "********** Deforris - Ubuntu Desktop Enviornment Setup **********"
+echo
+echo "********** Starting apt update. **********"
+apt update -y
+echo "********** Finished apt update. **********"
+echo
+echo "********** Starting apt upgrade. **********"
+apt upgrade -y
+echo "********** Finished apt upgrade. **********"
+echo
+echo "********** Starting apt autoremove. **********"
+apt autoremove -y
+echo "********** Finished apt autoremove. **********"
+echo
+echo "********** Starting install software-properties-common. **********"
+apt install software-properties-common
+echo "********** Finished install software-properties-common. **********"
+echo
+echo "********** Press Enter To Continue To git Setup... **********"
+read
 bash git.sh
