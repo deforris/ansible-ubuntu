48dfd8782e5097e24bfc245537682d7ae46db703
Jesse Muniz <142850309+deforris@users.noreply.github.com>
2023-11-09 07:29:00 -0500

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -       
+#title         :init
+#description   :A clean starting point on automatic install.
+#author		     :deforris
+#date          :202301109
+#version       :1.0.0
+#usage		     :bash init.sh
+#notes         :Starts git install and setup stuff which is not covered by ansible.
+#==============================================================================
+bash git.sh
