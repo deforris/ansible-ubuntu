0d86ef441dd22018c0e7b6446ff47074cc956e74
Jesse Muniz <142850309+deforris@users.noreply.github.com>
2023-11-14 04:12:17 -0500

    Create logger.sh

---
+++ logger.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -       
+#title         :init
+#description   :A clean starting point on automatic install.
+#author		     :cubicrace
+#date          :202301114
+#version       :1.0.0
+#usage		     :source ./logger.sh
+#notes         :Logging functionality to allow for persistance in install. Does not steal stdout yet.
+#credit        :https://www.cubicrace.com/2016/03/efficient-logging-mechnism-in-shell.html
+#==============================================================================
+
+SCRIPT_LOG=/home/cubicrace/SystemOut.log
+touch $SCRIPT_LOG
+
+function SCRIPTENTRY(){
+ timeAndDate=`date`
+ script_name=`basename "$0"`
+ script_name="${script_name%.*}"
+ echo "[$timeAndDate] [DEBUG]  > $script_name $FUNCNAME" >> $SCRIPT_LOG
+}
+
+function SCRIPTEXIT(){
+ script_name=`basename "$0"`
+ script_name="${script_name%.*}"
+ echo "[$timeAndDate] [DEBUG]  < $script_name $FUNCNAME" >> $SCRIPT_LOG
+}
+
+function ENTRY(){
+ local cfn="${FUNCNAME[1]}"
+ timeAndDate=`date`
+ echo "[$timeAndDate] [DEBUG]  > $cfn $FUNCNAME" >> $SCRIPT_LOG
+}
+
+function EXIT(){
+ local cfn="${FUNCNAME[1]}"
+ timeAndDate=`date`
+ echo "[$timeAndDate] [DEBUG]  < $cfn $FUNCNAME" >> $SCRIPT_LOG
+}
+
+
+function INFO(){
+ local function_name="${FUNCNAME[1]}"
+    local msg="$1"
+    timeAndDate=`date`
+    echo "[$timeAndDate] [INFO]  $msg" >> $SCRIPT_LOG
+}
+
+
+function DEBUG(){
+ local function_name="${FUNCNAME[1]}"
+    local msg="$1"
+    timeAndDate=`date`
+ echo "[$timeAndDate] [DEBUG]  $msg" >> $SCRIPT_LOG
+}
+
+function ERROR(){
+ local function_name="${FUNCNAME[1]}"
+    local msg="$1"
+    timeAndDate=`date`
+    echo "[$timeAndDate] [ERROR]  $msg" >> $SCRIPT_LOG
+}
